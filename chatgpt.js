fa54f229363f4b13914b71c66d0be5be7b0f1eec
T. Ehrler <30443689+TMelchiorE@users.noreply.github.com>
2023-05-10 15:21:42 -0400

    Update chatgpt.js

--- chatgpt.js
+++ chatgpt.js
@@ -6,22 +6,34 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 // check args for function/input
-if (argv.q){
-    chatContent();
+if (argv.c){
+    chat();
+}
+else if (argv.l)
+{
+    listmodels();
 }
 else {
     console.log("***try adding these params***")
-    console.log("   -q='ask a question'")
+    console.log("   -c='chat question'") 
+    console.log("   -l  (list available models)")  
 }
 
 // sends general question -> gets answer
-async function chatContent(){
+async function chat(){
 
     const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: argv.q}],
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: argv.c}],
     });
-    //console.log(completion.data.choices[0].message);
+    
     console.log(completion.data.choices[0].message.content);
 
+}
+
+// list available models
+async function listmodels(){
+
+    const response = await openai.listModels();
+    console.log(response.data);
 }
